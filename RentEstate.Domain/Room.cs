5201ec18e21cbea20aea0804c2282462fa59ee0a
DenisSuperStar <DabBr1994@yandex.ru>
2023-05-02 19:49:05 +0300

    1. Сконфигуририровал модели с помщью EntityTypeBuilder 2. Указал обязательные своства модели с помощью FluentAPI

--- RentEstate.Domain/Room.cs
+++ RentEstate.Domain/Room.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RentEstate.Domain
+﻿namespace RentEstate.Domain
 {
     public class Room : Flat
     {
