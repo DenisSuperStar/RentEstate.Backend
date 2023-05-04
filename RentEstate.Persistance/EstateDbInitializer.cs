885fde6e14ca41294102d20764ea26f55efce1e1
DenisSuperStar <DabBr1994@yandex.ru>
2023-05-04 23:56:07 +0300

    Написал класс для инициализации базы данных недвижимости

---
+++ RentEstate.Persistance/EstateDbInitializer.cs
@@ -0,0 +1,12 @@
+﻿
+using RentEstate.Persistance;
+
+namespace Notes.Persistance
+{
+    public class EstateDbInitializer
+    {
+        public static void Initialize(EstateDbContext context) {
+            context.Database.EnsureCreated();
+        }
+    }
+}
